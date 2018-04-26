43a654ffcfe182a30d749f6086e7f05c2cfdc466
github username <github email address>
2018-04-26 19:18:14 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" # Parent Class being inherited from
+
+class Car < Vehicle
+  def go # This is overwriting the same method from the Parent Class
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
